dc1380b0087ff111051ceeda85991facca88e7e9
joey <zhaoyi0113@gmail.com>
2021-06-03 21:50:44 +1000

    fix alb service account permission

--- bin/deploy_alb_ctr.sh
+++ bin/deploy_alb_ctr.sh
@@ -8,7 +8,7 @@ echo "create for $CLUSTER_NAME"
 eksctl create iamserviceaccount \
   --cluster=$CLUSTER_NAME \
   --namespace=kube-system \
-  --name=aws-load-balancer-controller \
+  --name=alb-ingress-controller \
   --attach-policy-arn=arn:aws:iam::264100014405:policy/AWSLoadBalancerControllerIAMPolicy-${CLUSTER_NAME} \
   --override-existing-serviceaccounts \
   --approve
