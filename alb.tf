1808c96900835adecbb4232fc07ff1e0a4e75570
joey <zhaoyi0113@gmail.com>
2021-06-08 14:46:53 +1000

    update service account role

---
+++ alb.tf
@@ -0,0 +1,31 @@
+data "aws_iam_policy_document" "elk_alb_sa" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.elk.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "elk_alb_sc" {
+  name = "eks-alb-sa-${var.eks_cluster_name}"
+
+  assume_role_policy = data.aws_iam_policy_document.elk_alb_sa.json
+
+  inline_policy {
+    name = "elk_alb_sc_policy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["log:*", "ec2:*", "iam:*", "elasticloadbalancing:*", "cognito-idp:*", "acm:*", "elasticfilesystem:*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
--- eks_cluster_fargate.tf
+++ eks_cluster_fargate.tf
@@ -79,7 +79,7 @@ resource "aws_iam_role_policy_attachment" "elk-AmazonEKSClusterPolicy" {
 }
 
 resource "aws_iam_role_policy_attachment" "elk-tag-policy" {
-  policy_arn = aws_iam_policy.policy.arn 
+  policy_arn = aws_iam_policy.policy.arn
   role       = aws_iam_role.elk.name
 }
 
@@ -105,23 +105,23 @@ resource "aws_iam_openid_connect_provider" "elk" {
   }
 }
 
-data "aws_iam_policy_document" "elk_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "elk_assume_role_policy" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect  = "Allow"
 
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.elk.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:aws-node"]
-    }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "${replace(aws_iam_openid_connect_provider.elk.url, "https://", "")}:sub"
+#       values   = ["system:serviceaccount:kube-system:aws-node"]
+#     }
 
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.elk.arn]
-      type        = "Federated"
-    }
-  }
-}
+#     principals {
+#       identifiers = [aws_iam_openid_connect_provider.elk.arn]
+#       type        = "Federated"
+#     }
+#   }
+# }
 
 # resource "aws_iam_role" "elk" {
 #   assume_role_policy = data.aws_iam_policy_document.elk_assume_role_policy.json
@@ -151,7 +151,7 @@ resource "aws_iam_role" "fargate_profile" {
       Version = "2012-10-17"
       Statement = [
         {
-          Action   = ["log:*", "ec2:*", "iam:*", "elasticloadbalancing:*", "cognito-idp:*", "acm:*", "elasticfilesystem:*" ]
+          Action   = ["log:*", "ec2:*", "iam:*", "elasticloadbalancing:*", "cognito-idp:*", "acm:*", "elasticfilesystem:*"]
           Effect   = "Allow"
           Resource = "*"
         },
