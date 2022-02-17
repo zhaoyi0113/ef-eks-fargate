d4ea1640415c97f1411614048351c754f90b88ac
Joey Zhao <zhaoyi0113@gmail.com>
2022-02-17 21:06:27 +1100

    update version

--- tf-elk/versions.tf
+++ tf-elk/versions.tf
@@ -5,25 +5,25 @@ terraform {
       version = ">= 3.20.0"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = ">= 3.0.0"
+    # }
 
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
+    # local = {
+    #   source  = "hashicorp/local"
+    #   version = ">= 2.0.0"
+    # }
 
-    null = {
-      source  = "hashicorp/null"
-      version = "3.0.0"
-    }
+    # null = {
+    #   source  = "hashicorp/null"
+    #   version = ">= 3.0.0"
+    # }
 
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
+    # template = {
+    #   source  = "hashicorp/template"
+    #   version = ">= 2.2.0"
+    # }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -31,6 +31,6 @@ terraform {
     }
   }
 
-  required_version = "~> 1.0.0"
+  required_version = ">= 1.0.0"
 }
 
